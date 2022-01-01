bb0f92e065c314efb04eb3ad28361981dd6ce94c
Jon Yoder <jon@yoder.cloud>
2022-01-01 13:19:48 -0500

    I *hate* Python packaging

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-setup(
-	name='pyoganesson',
-	version='0.1',
-	description='Pure Python implementation of the Oganesson messaging framework',
-	url='https://github.com/darkwyrm/pyoganesson',
-	author='Jon Yoder',
-	author_email='jon@yoder.cloud',
-	license='MIT',
-	packages=find_packages(),
-	classifiers=[
-		"Development Status :: 2 - Pre-Alpha",
-		"Intended Audience :: Developers",
-		"Topic :: Communications",
-		"Programming Language :: Python :: 3.5",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
-	python_requires='>=3.5',
-	install_requires=[
-		'PyNaCl>=1.3.0',
-		'pycryptostring>=1.0.0',
-		'retval>=1.0.0'
-	]
-)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
