bd2db2da1d6de9bd44f1a2ab43edc31dc0a1716a
Jon Yoder <jon@yoder.cloud>
2022-01-01 10:46:21 -0500

    Rework existing code

--- pyoganesson/field.py
+++ pyoganesson/field.py
@@ -1,71 +1,75 @@
 from retval import RetVal, ErrBadType
 
 # The DataField structure is the foundation of the lower levels of Oganesson messaging and is used
-# for data serialization. The serialized format consists of a type code, a uint16 length, and a
+# for data serialization. The serialized format consists of a type code, a UInt16 length, and a
 # byte array of up to 64K.
 
-DFUnknownType = 0
-DFInt8Type = 1
-DFInt16Type = 2
-DFInt32Type = 3
-DFInt64Type = 4
-DFUInt8Type = 5
-DFUInt16Type = 6
-DFUInt32Type = 7
-DFUInt64Type = 8
-DFStringType = 9
-DFBoolType = 10
-DFFloat32Type = 11
-DFFloat64Type = 12
-DFByteType = 13
-
-def get_type_size(t: int) -> int:
-	'''get_type_size() returns the number of bytes occupied by the specified field type.
-
-	For fields which have a variable size, such as byte arrays or strings, 0 is returned. -1 is 
-	returned for invalid types.
-	'''
-
-	if t in [DFInt8Type, DFUInt8Type, DFBoolType]:
-		return 1
-	if t in [DFInt16Type, DFUInt16Type]:
-		return 2
-	if t in [DFInt32Type, DFUInt32Type]:
-		return 4
-	if t in [DFInt64Type, DFUInt64Type]:
-		return 8
-	
-	return -1
-
-def is_valid_field_type(t: int) -> bool:
-	'''Returns true if the specified value is a valid DataField type code.
-	
-	Because the Unknown type is treated as an error condition, passing DFUnknownType to this 
-	function will result in False being returned.
-	'''
-
-	return t > DFUnknownType and t <= DFByteType
-
-
-# Maps are just a series of DataFields. The payloads themselves are uint16's
-# containing the number of items to follow that belong to the container. For maps, the item
-# count is half of the number of actual DataFields to follow -- a DataField map consists of
-# a list of pairs of a DFStringType and another field. For complexity reasons maps and lists
-# may not be nested.
-DFMapType = 14
-
-# Message codes are strings, but they need to be different from the string type for clarity
-DFMsgCodeType = 15
-
 class DataField:
-	'''DataField is the foundation for the Oganesson wire message data serialization format'''
+	'''The DataField class manages the message type codes and associated data sizes'''
+	Unknown = 0
+	Int8 = 1
+	Int16 = 2
+	Int32 = 3
+	Int64 = 4
+	UInt8 = 5
+	UInt16 = 6
+	UInt32 = 7
+	UInt64 = 8
+	String = 9
+	Bool = 10
+	Float32 = 11
+	Float64 = 12
+	Byte = 13
 	
-	def __init__(self):
-		self.type = 0
-		self.value = list()
+	# Maps are just a series of DataFields. The payloads themselves are UInt16's
+	# containing the number of items to follow that belong to the container. The actual item
+	# count is half of the number of actual DataFields to follow -- a DataField map item is a 
+	# string field paired with another field. 
+	Map = 14
 
-	def set(self, type_code: int, v: any) -> RetVal:
+	# Message codes are strings, but they need to be different from the string type for clarity
+	MsgCode = 15
+
+	def __init__(self, field_type = DataField.Unknown, field_value = None):
+		self.type = field_type
+		self.value = field_value
+	
+	def get_flat_size(self) -> int:
+		'''get_flat_size() returns the number of bytes occupied by the field when serialized.
 		
-		# TODO: implement DataField.set()
+		A negative value is returned if there is an error
+		'''
+
+		if self.type in [DataField.Byte, DataField.String, DataField.MsgCode]:
+			# Strings and Byte arrays are limited to 65535 bytes. Considering these messages are
+			# lightweight, that should be plenty.
+			value_length = min(65535, len(self.value))
+			return 3+len(value_length)
 		
-		return RetVal()
+		if self.type in [DataField.Int8, DataField.UInt8, DataField.Bool]:
+			return 1+3
+		
+		if self.type in [DataField.Int16, DataField.UInt16, DataField.Map]:
+			return 2+3
+		
+		if self.type in [DataField.Int32, DataField.UInt32, DataField.Float32]:
+			return 4+3
+		
+		if self.type in [DataField.Int64, DataField.UInt64, DataField.Float64]:
+			return 8+3
+		
+		return -1
+
+	def is_valid(self) -> bool:
+		'''Returns true if the specified value is a valid DataField type code.
+		
+		Because the Unknown type is treated as an error condition, passing unknown to this 
+		function will result in False being returned.
+		'''
+
+		# Because get_flat_size() has to have a case to handle every type of field, if we get a
+		# negative value, it's because the field type is invalid.
+		flat_size = self.get_flat_size()
+		
+		return flat_size > 0 and len(self.value) == flat_size - 3
+
