e4522b15bb351c9979e2343585a986e21bdf60f3
Jon Yoder <jon@yoder.cloud>
2022-01-08 11:32:43 -0500

    Add passing test for writing multipart messages

--- tests/unit/test_packetsession.py
+++ tests/unit/test_packetsession.py
@@ -12,7 +12,7 @@ def funcname() -> str:
 	return frames[1].function
 
 
-def test_write_wire_packet():
+def test_read_write_wire_packet():
 	'''Tests basic write_wire_packet() functionality'''
 	sock = FakeSocket()	
 
@@ -33,25 +33,38 @@ def test_write_wire_packet():
 
 def test_write_multipart_wire_packet():
 	'''Tests PacketSession write_wire_packet()'''
+	
 	sock = FakeSocket()	
 
+	# Normally changing the session's max packet size is not recommended, but in this case it's 
+	# necessary to make the test data manageable.
 	sender = PacketSession(sock)
-	msg = DataField('singlepacket',b'{"Type":"TEST","Command":"ThisIsATestMessage",' \
-		b'"Args": {"TestArg": "AAAAAAAAAABBBBBBBBBBCCCCCCCCCCDDDDDDDDDDEEEEEEEEEEFFFFFFFFFF' \
-		b'GGGGGGGGGGHHHHHHHHHHIIIIIIIIIIJJJJJJJJJJKKKKKKKKKKLLLLLLLLLLMMMMMMMMMMNNNNNNNNNN' \
-		b'OOOOOOOOOOPPPPPPPPPPQQQQQQQQQQRRRRRRRRRRSSSSSSSSSSTTTTTTTTTTUUUUUUUUUUVVVVVVVVVV' \
-		b'WWWWWWWWWWXXXXXXXXXXYYYYYYYYYYZZZZZZZZZZAAAAAAAAAABBBBBBBBBBCCCCCCCCCCDDDDDDDDDD' \
-		b'EEEEEEEEEEFFFFFFFFFFGGGGGGGGGGHHHHHHHHHHIIIIIIIIIIJJJJJJJJJJKKKKKKKKKKLLLLLLLLLL' \
-		b'MMMMMMMMMMNNNNNNNNNNOOOOOOOOOOPPPPPPPPPPQQQQQQQQQQRRRRRRRRRRSSSSSSSSSSTTTTTTTTTT' \
-		b'UUUUUUUUUUVVVVVVVVVVWWWWWWWWWWXXXXXXXXXXYYYYYYYYYYZZZZZZZZZZ12345678901234567890"}}')
+	sender.maxsize = 10
+	msg = DataField('singlepacket',b'ABCDEFGHIJKLMNOPQRS')
 	status = sender.write_wire_packet(msg)
 	assert not status.error(), f'{funcname()}: error sending multipart msg: {status.error()}'
 
-	receiver = PacketSession(sock)
-	status = receiver.read_wire_packet()
-	assert not status.error(), f"{funcname()}: failed to read multipart message: {status.error()}"
+	# Because the maximum buffer size is only 10 bytes, there should be room for only 7 letters per
+	# message because of the 3-byte overhead for message headers.
+
+	# The first message should contain the total size of the data
+	firstmsg = b'\x16\x00\x02\x00\x13'
+	assert sock.buffer[:5] == firstmsg, \
+		f"{funcname()}: first message data mismatch: {sock.buffer[:5]}"
+	
+	secondmsg = b'\x17\x00\x07ABCDEFG'
+	assert sock.buffer[5:15] == secondmsg, \
+		f"{funcname()}: second message data mismatch: {sock.buffer[5:15]}"
+
+	thirdmsg = b'\x17\x00\x07HIJKLMN'
+	assert sock.buffer[15:25] == thirdmsg, \
+		f"{funcname()}: third message data mismatch: {sock.buffer[15:25]}"
+
+	finalmsg = b'\x18\x00\x05OPQRS'
+	assert sock.buffer[25:] == finalmsg, \
+		f"{funcname()}: final message data mismatch: {sock.buffer[25:]}"
 
 
 if __name__ == '__main__':
-	test_write_wire_packet()
+	test_read_write_wire_packet()
 	test_write_multipart_wire_packet()
