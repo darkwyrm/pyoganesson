3f436e6ffa0ca9ebabd3a181e01f9ffe44d089ba
Jon Yoder <jon@yoder.cloud>
2022-01-08 11:41:06 -0500

    Convert FakeSocket internal storage - Use a list so that multiple messages can be stored at the same time

--- tests/unit/fakesocket.py
+++ tests/unit/fakesocket.py
@@ -3,36 +3,22 @@ class FakeSocket:
 	'''FakeSocket is a stand-in for a real socket used to avoid async-await headaches'''
 
 	def __init__(self):
-		self.buffer = None
+		self.buffer = []
 		self.flags = None
 
-	def send(self, data: bytes, _=0) -> int:
+	def send(self, data: bytes) -> int:
 		'''Writes the data to the fake socket'''
-		if self.buffer:
-			self.buffer = self.buffer + data
-		else:
-			self.buffer = data
+		self.buffer.append(data)
 		
 		return len(data)
 
-	def recv(self, bufsize=0, _=0) -> bytes:
+	def recv(self) -> bytes:
 		'''Reads data from the fake socket'''
 		
 		if not self.buffer:
 			return None
 		
-		if bufsize:
-			outsize = min(bufsize, len(self.buffer))
-		else:
-			outsize = len(self.buffer)
-		
-		out = self.buffer[:outsize]
-		if len(self.buffer) > outsize:
-			self.buffer = self.buffer[outsize:]
-		else:
-			self.buffer = None
-		
-		return out
+		return self.buffer.pop(0)
 
 	def settimeout(self, _: float):
 		'''settimeout is a no-op for socket compatibility'''
--- tests/unit/test_packetsession.py
+++ tests/unit/test_packetsession.py
@@ -13,7 +13,7 @@ def funcname() -> str:
 
 
 def test_read_write_wire_packet():
-	'''Tests basic write_wire_packet() functionality'''
+	'''Tests basic wire packete reading and writing functionality'''
 	sock = FakeSocket()	
 
 	sender = PacketSession(sock)
@@ -32,7 +32,7 @@ def test_read_write_wire_packet():
 
 
 def test_write_multipart_wire_packet():
-	'''Tests PacketSession write_wire_packet()'''
+	'''Tests sending multipart messages with PacketSession's write_wire_packet()'''
 	
 	sock = FakeSocket()	
 
@@ -49,22 +49,42 @@ def test_write_multipart_wire_packet():
 
 	# The first message should contain the total size of the data
 	firstmsg = b'\x16\x00\x02\x00\x13'
-	assert sock.buffer[:5] == firstmsg, \
-		f"{funcname()}: first message data mismatch: {sock.buffer[:5]}"
+	assert sock.buffer[0] == firstmsg, \
+		f"{funcname()}: first message data mismatch: {sock.buffer[0]}"
 	
 	secondmsg = b'\x17\x00\x07ABCDEFG'
-	assert sock.buffer[5:15] == secondmsg, \
-		f"{funcname()}: second message data mismatch: {sock.buffer[5:15]}"
+	assert sock.buffer[1] == secondmsg, \
+		f"{funcname()}: second message data mismatch: {sock.buffer[1]}"
 
 	thirdmsg = b'\x17\x00\x07HIJKLMN'
-	assert sock.buffer[15:25] == thirdmsg, \
-		f"{funcname()}: third message data mismatch: {sock.buffer[15:25]}"
+	assert sock.buffer[2] == thirdmsg, \
+		f"{funcname()}: third message data mismatch: {sock.buffer[2]}"
 
 	finalmsg = b'\x18\x00\x05OPQRS'
-	assert sock.buffer[25:] == finalmsg, \
-		f"{funcname()}: final message data mismatch: {sock.buffer[25:]}"
+	assert sock.buffer[3] == finalmsg, \
+		f"{funcname()}: final message data mismatch: {sock.buffer[3]}"
+
+
+def test_read_multipart_wire_packet():
+	'''Tests receiving multipart messages with PacketSession's read_wire_packet()'''
+	
+	sock = FakeSocket()	
+
+	# Normally changing the session's max packet size is not recommended, but in this case it's 
+	# necessary to make the test data manageable.
+	sender = PacketSession(sock)
+	sender.maxsize = 10
+	msg = DataField('singlepacket',b'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+	status = sender.write_wire_packet(msg)
+	assert not status.error(), f'{funcname()}: error sending multipart msg: {status.error()}'
+
+	receiver = PacketSession(sock)
+	status = receiver.read_wire_packet()
+	assert not status.error(), \
+		f"{funcname()}: error receiving multipart message: {status.error()}"
 
 
 if __name__ == '__main__':
 	test_read_write_wire_packet()
 	test_write_multipart_wire_packet()
+	test_read_multipart_wire_packet()
