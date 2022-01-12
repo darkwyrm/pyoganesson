282580f338ff92a74a25878c3e054f3cd08d85ce
Jon Yoder <jyoder@bcadmin.com>
2022-01-12 13:22:33 -0500

    Add a bit more to Setup()

--- pyoganesson/session.py
+++ pyoganesson/session.py
@@ -3,18 +3,28 @@ import socket
 from pyeznacl import SecretKey
 from retval import RetVal
 
+from field import DataField
+from packetsession import PacketSession
+
 class OgServer:
 	'''Handles the server side of an encrypted Oganesson messaging session'''
 
 	def __init__(self, conn: socket.socket, fingerprint: str):
-		self.conn = conn
+		self.session = PacketSession(conn)
 		self.key = SecretKey()
 		self.nextkey = None
 		self.handlers = {}
 		self.fingerprint = fingerprint		
 
-	def Setup() -> RetVal:
+	def Setup(self) -> RetVal:
 		'''Handles the server side of setting up an encrypted comms channel with a client'''
 		
 		# Once the initial TCP connection is established, the client is expected to ask for the type
 		# of session encryption expected. Anything else is an error.
+
+		status = self.session.read_wire_packet()
+		if status.error():
+			return status
+
+		df = status['field']
+		
