c37a2d964729d36dacf4e79dd8b8bd3c755e872f
Jon Yoder <jyoder@bcadmin.com>
2022-01-20 10:44:07 -0500

    Finish implementing tests for unflatten()

--- tests/unit/test_wiremsg.py
+++ tests/unit/test_wiremsg.py
@@ -87,7 +87,20 @@ def test_wiremsg_flatten_unflatten():
 		f"{funcname()}: flat populated message data mismatch.\n" + \
 			f"Want:  {flatdata}\nGot: {status['bytes']}"
 
-	# TODO: Finish test_wiremsg_flatten()
+	wm = WireMsg()
+	flatdata = b'\x0f\x00\x04test\x0e\x00\x05\x06\x00\x02\x00\x00'
+	status = wm.unflatten(flatdata)
+	assert not status.error(), f"{funcname()}: error unflattening empty wire message: {status.error()}"
+	assert wm.code == 'test' and len(wm.attachments) == 0, \
+		f"{funcname()}: unflattened empty message data mismatch"
+
+	wm = WireMsg()
+	flatdata = b'\x0f\x00\x04test\x0e\x00\r\x06\x00\x02\x00\x01\t\x00\x011\t\x00\x01a'
+	status = wm.unflatten(flatdata)
+	assert not status.error(), \
+		f"{funcname()}: error unflattening wire message with data: {status.error()}"
+	assert wm.code == 'test' and len(wm.attachments) == 1 and '1' in wm.attachments and \
+		wm.attachments['1'] == 'a', f"{funcname()}: unflattened empty message data mismatch"
 
 
 if __name__ == '__main__':
