495a05518a26dc0c41b7bc6a2f03d88d8c638f39
Jon Yoder <jon@yoder.cloud>
2022-01-22 12:03:56 -0500

    Rename parameter 'index' to 'name'

--- pyoganesson/wiremsg.py
+++ pyoganesson/wiremsg.py
@@ -10,24 +10,24 @@ class WireMsg:
 		self.code = msgcode
 		self.attachments = {}
 	
-	def add_field(self, index: str, data: any, dtype='') -> RetVal:
+	def add_field(self, name: str, data: any, dtype='') -> RetVal:
 		'''Method for attaching data to the message
 
 		Parameters:
-		index: the name of the field to attach
+		name: the name of the field to attach
 		data: the data to attach as the field value
 
 		Returns:
 		error codes only
 		'''
-		if not index:
+		if not name:
 			return RetVal(ErrEmptyData)
 		
-		if not isinstance(index, str):
+		if not isinstance(name, str):
 			return RetVal(ErrBadType, 'field indices must be strings')
 		
 		if data is None:
-			del self.attachments[index]
+			del self.attachments[name]
 		else:
 			df = DataField()
 			if dtype:
@@ -37,11 +37,11 @@ class WireMsg:
 			if status.error():
 				return status
 			
-			self.attachments[index] = df
+			self.attachments[name] = df
 		
 		return RetVal()
 	
-	def get_field(self, index: str) -> RetVal:
+	def get_field(self, name: str) -> RetVal:
 		'''Returns the value of the specified attachment
 		
 		Parameters:
@@ -52,31 +52,31 @@ class WireMsg:
 		field 'value': the value of the field
 		'''
 
-		if not index or len(self.attachments) == 0:
+		if not name or len(self.attachments) == 0:
 			return RetVal(ErrEmptyData)
 		
-		if index not in self.attachments:
+		if name not in self.attachments:
 			return RetVal(ErrNotFound)
 		
-		return self.attachments[index].get()
+		return self.attachments[name].get()
 	
-	def has_field(self, index: str) -> bool:
+	def has_field(self, name: str) -> bool:
 		'''Returns true if the message has the specified field'''
-		return index in self.attachments
+		return name in self.attachments
 	
-	def get_string_field(self, index: str) -> str:
+	def get_string_field(self, name: str) -> str:
 		'''Convenience method for working with string fields
 		
 		Parameters:
-		index: the name of the attachment to obtain
+		name: the name of the attachment to obtain
 		
 		Returns:
 		Value of the specified string field or empty string on error.'''
 
-		if not index or not index in self.attachments:
+		if not name or not name in self.attachments:
 			return ''
 
-		status = self.attachments[index].get()
+		status = self.attachments[name].get()
 		if status.error() or status['type'] not in ['string', 'msgcode']:
 			return ''
 
